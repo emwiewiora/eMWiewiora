2d52d61c2fe2379309e10140632b087f245bfd06
emwiewiora <ewiewiora@gmail.com>
2020-08-02 09:17:54 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -10,23 +10,23 @@ res = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}')
 result = json.loads(res.text)
 f.write(f'''<img align="right" src="{result['sprites']['front_default']}" width="100" height="100">
 <p>I am currently learning FullStack part time, in hopes to change my career.</p>
-<h2>🌱 I’m currently learning ...</h2>
+<h3>🌱 I’m currently learning ...</h3>
 <ol>
     <li>More indepth HTML</li>
     <li>More indepth CSS</li>
     <li>The basics of JavaScript</li>
 </ol>
 
-<h2>🔭 I’m currently working on ...</h2>
+<h3>🔭 I’m currently working on ...</h3>
 <ol>
-    <li>see what i am playing with in codepen.io <a target="_blank" href="https://codepen.io/emwiewiora">here</a></li>
+    <li>See what i am playing with in codepen.io <a target="_blank" href="https://codepen.io/emwiewiora">here</a></li>
     <li>I have started <a href="https://www.theodinproject.com/">The Odin Project</a></li>
+    <li>Developing this page</li>
 </ol>
-<p>This page is a work in progress.</p>
 
 <p>Thanks for visiting.....</p>
 
-<h3>⚡ You can also find me here...</h3>
+<h4>⚡ You can also find me here...</h4>
 <ul>
     <li><a href="https://www.linkedin.com/in/ewiewiora">LinkedIn</a></li>
     <li><a href="https://twitter.com/emwiewiora">Twitter</a></li>
