25d72b4464d099f786d0208fbf96fc9b1236f07e
emwiewiora <ewiewiora@gmail.com>
2020-08-02 08:27:22 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,8 +4,33 @@ f = open("./README.md", "w")
 pokemon_id = random.randint(1, 151)
 res = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}')
 result = json.loads(res.text)
-f.write(f'''<p align="center">
-    <img src="{result['sprites']['front_default']}" width="150" height="150">
+f.write(f'''
+<p>I am currently learning FullStack part time, in hopes to change my career.</p>
+<h2>🌱 I’m currently learning ...</h2>
+<ol>
+    <li>1. More indepth HTML</li>
+    <li>2. More indepth CSS</li>
+    <li>3. The basics of JavaScript</li>
+</ol>
+
+<h2>🔭 I’m currently working on ...</h2>
+<ol>
+    <li>1. see what i am playing with in codepen.io <a target="_blank" href="https://codepen.io/emwiewiora">here</a></li>
+    <li>2. I have started <a href="https://www.theodinproject.com/">The Odin Project</a></li>
+</ol>
+<p>This page is a work in progress.</p>
+
+<p>Thanks for visiting.....</p>
+
+<h3>⚡ You can also find me here...</h3>
+<ul>
+    <li><a href="https://www.linkedin.com/in/ewiewiora">LinkedIn</a></li>
+    <li><a href="https://twitter.com/emwiewiora">Twitter</a></li>
+    <li><a href="https://www.reddit.com/user/efreaq">Reddit</a>(as efreaq)</li>
+</ul>
+
+<p align="center">
+    <img src="{result['sprites']['front_default']}" width="50" height="50">
 </p>
 <h3 align="center">You have been greeted by - <b>{result['name'].title()}</b></h3>
 <h3 align="center">Have a nice day!</h3>
